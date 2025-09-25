0735272229bed1e80b8af9cf114d5c99452b7049
Prachestita04 <prachestitamahapatra@gmail.com>
2025-09-25 12:49:34 +0530

    dynamic web calendar by js programs

--- src/main/java/SelTestProgram/HandleDynamicCalenderByJS.java
+++ src/main/java/SelTestProgram/HandleDynamicCalenderByJS.java
@@ -10,7 +10,6 @@ import java.time.Duration;
 public class HandleDynamicCalenderByJS {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new EdgeDriver();
-        driver = new EdgeDriver();
         driver.get("https://www.spicejet.com/");
 
         driver.manage().window().maximize();
@@ -19,12 +18,12 @@ public class HandleDynamicCalenderByJS {
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
-        WebElement departureDate = driver.findElement(By.xpath("//div[contains(@data-testid,'departure-date')]//div[text()='Departure Date']"));
+        WebElement departureDate = driver.findElement(By.xpath("//div[contains(@data-testid,'departure-date-dropdown-label-test-id')]//div[contains(text(),'Thu, 2 Oct 2025')]"));
         System.out.println("Before wait");
         WebDriverWait wait =new WebDriverWait(driver,Duration.ofSeconds(40));
         wait.ignoring(StaleElementReferenceException.class).until(ExpectedConditions.visibilityOf(departureDate));
         System.out.println("After wait");
-        String requiredDate = "Tue, 23 Sep 2025";
+        String requiredDate = "Tue, 31 Sep 2025";
 //        departureDate.sendKeys(requiredDate);
         inputDataBJS(driver,departureDate,requiredDate);
         Thread.sleep(5000);
@@ -32,7 +31,14 @@ public class HandleDynamicCalenderByJS {
         driver.quit();
     }
     private static void inputDataBJS(WebDriver driver,WebElement element,String string){
-        JavascriptExecutor js = ((JavascriptExecutor) driver);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].innerText='"+string+"';",element); //(arguments[0].innerText='webelementvalue';element)
     }
+
+    //don't use below
+//    private static void inputDataBJS(WebDriver driver,WebElement element,String string){
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("arguments[0].setAttribute('value','" + string + "');",element); //(arguments[0].setAttribute('value','webelementvalue');
+//    }
+
 }
